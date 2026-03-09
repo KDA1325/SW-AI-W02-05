6a09f406769d37b777f111c313b689b9254c106e
kda <1325680q@gmail.com>
2026-03-09 23:43:15 +0900

    08_bubble_sort.py PASS

--- week2/basic/08_bubble_sort.py
+++ week2/basic/08_bubble_sort.py
@@ -34,6 +34,8 @@ def bubble_sort(arr):
     """
     n = len(arr)
     
+    tmp = 0
+
     # TODO: 외부 반복문 - n-1번 반복
     # 각 패스마다 가장 큰 원소가 끝으로 이동
     ## TODO: 내부 반복문 - 인접한 원소 비교
@@ -41,8 +43,12 @@ def bubble_sort(arr):
     ## TODO: 인접한 두 원소 비교 및 교환
     ## arr[j] > arr[j+1]이면 교환
     ## 외부 반복문: n-1번 실행
-    pass
-        
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                tmp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tmp
     return arr
 
 def bubble_sort_optimized(arr):
@@ -62,11 +68,17 @@ def bubble_sort_optimized(arr):
         
         # TODO: 내부 반복문과 교환 로직 구현
         # 교환이 발생하면 swapped = True 설정        
-        pass
-        
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                tmp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tmp
 
+                swapped = True
+    
         # TODO: 교환이 없으면 이미 정렬된 것이므로 break
-        pass
+            else:
+                break
 
     return arr
 
