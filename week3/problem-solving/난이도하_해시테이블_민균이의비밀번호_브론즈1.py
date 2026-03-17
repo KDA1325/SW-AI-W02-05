b5dc65960bb4c7aad390e3e7020740cf32e1762d
kda <1325680q@gmail.com>
2026-03-17 20:12:58 +0900

    난이도하_해시테이블_민균이의비밀번호_브론즈1.py PASS

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,27 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+
+def find_word(words):
+    for word in words:
+        if len(word) 
+        if word[::-1] in words:
+            return word
+
+
+if __name__ == '__main__':
+    n = int(input())
+    words = set()
+
+    for i in range(n):
+        word = str(input())
+        words.add(word)
+
+    result = find_word(words)
+
+    result_len = len(result)
+
+    mid = result_len // 2
+    result_s = result[mid]
+
+    print(result_len)
+    print(result_s)
