c62e2407d5b29fe8b98253c7ac702f3fb51ff283
kda <1325680q@gmail.com>
2026-03-28 00:05:23 +0900

    난이도하_DP_01타일_실버3.py PASS

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -6,16 +6,14 @@ output: 길이가 n인 모든 2진 수열의 개수를 15746으로 나눈 나머
 """
 
 def solution(n):
-    # dp = [0] * (n+1)
-    dp = []
+    dp = [0] * (n+1)
     dp[1] = 1
 
     if n >= 2:
         dp[2] = 2
 
     for i in range(3, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]
-        #result = dp[i] % 15746  
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746  
 
     return dp[n] % 15746   
 
