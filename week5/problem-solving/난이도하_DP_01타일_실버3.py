c4ab75511afe4bc488a835a9eda4875ddf332d37
kda <1325680q@gmail.com>
2026-03-28 00:04:14 +0900

    난이도하_DP_01타일_실버3.py PASS

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,25 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+"""
+input: n
+output: 길이가 n인 모든 2진 수열의 개수를 15746으로 나눈 나머지를 출력
+"""
+
+def solution(n):
+    # dp = [0] * (n+1)
+    dp = []
+    dp[1] = 1
+
+    if n >= 2:
+        dp[2] = 2
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+        #result = dp[i] % 15746  
+
+    return dp[n] % 15746   
+
+if __name__ == '__main__':
+    n = int(input())
+
+    print(solution(n))
