ed7b050d69fcf0b7d1628a02d5ef68c014162642
kda <1325680q@gmail.com>
2026-03-29 00:11:05 +0900

    난이도하_그리디_동전0_실버4.py PASS

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,30 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+def greedy(coins, k):
+    memo = {}
+    count = 0
+    result = 0
+    
+    coins.sort(reverse=True)
+    
+    for coin in coins:
+        count = k // coin
+
+        if count > 0:
+            result += count
+            
+            k = k % coin
+            
+    return result
+
+if __name__ == '__main__':
+    n, k = map(int, input().split())
+
+    coins = []
+
+    for i in range(n):
+        coin = int(input())
+        
+        coins.append(coin)
+
+    print(greedy(coins, k))
