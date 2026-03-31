9854459b131b64ccf32a0d49bcd8739ef0188b88
kda <1325680q@gmail.com>
2026-04-01 01:45:53 +0900

    난이도하_그리디_잃어버린괄호_실버2.py PASS

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,21 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+"""
+수식
+가장 처음과 마지막 문자는 숫자
+연속해서 두 개 이상의 연산자가 나타나지 않음
+5자리보다 많이 연속되는 숫자는 없음
+수는 0으로 시작할 수 있음
+"""
+def solution(ins):
+    first = sum(map(int, ins[0].split('+')))
+
+    for part in ins[1:]:
+        first -= sum(map(int, part.split('+')))
+
+    return first
+    
+if __name__ == '__main__':
+    ins = input().split('-')
+
+    print(solution(ins))
